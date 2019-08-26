6354f36e366ce138a7318870f48fcf9fc0812a6b
WaniFi <chaos0824@naver.com>
2019-08-26 14:10:46 +0900

    팔 오브젝트 객체 및 팔 컨트롤러 스크립트 구현

---
+++ Hand.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Hand : MonoBehaviour
+{
+    /* 특정 상태의 핸드 명 */
+    public string handName;
+    /* 공격 범위 */
+    public float range;
+    /* 공격 피해량 */
+    public int damage;
+    /* 손으로 일하는 속도 */
+    public float workSpeed;
+    /* 공격 한 번당 지연되는 시간 */
+    public float attackDelay;
+    /* 공격 활성화 시점 */
+    public float attackDelayA;
+    /* 공격 비활성화 시점 */
+    public float attackDelayB;
+    /* 공격 기준을 핸드가 아닌 화면으로 지정하기 위한 애니메이션 */
+    public Animator anim;
+
+    /*
+     * Start, Update 함수는 있는 것 만으로도 자원을 소모함
+     */
+}
---
+++ HandController.cs
@@ -0,0 +1,99 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HandController : MonoBehaviour
+{
+    [SerializeField]
+    /* 현재 장착된 Hand 타입 무기 */
+    private Hand currentHand;
+
+    /* 공격 상태 파악 변수 */
+    private bool isAttack;
+    /* 팔 휘두름 상태 변수 */
+    private bool isSwing;
+
+    /*
+     * Raycast 레이저를 통해 맞는 오브젝트의 정보를 가져울 수 있음
+     * 그 때 사용하기 위한 변수
+     */
+    private RaycastHit hitInfo;
+
+    // Update is called once per frame
+    void Update()
+    {
+        /* 착용한 Hand를 통한 공격 시도 */
+        TryAttack();
+    }
+
+    private void TryAttack()
+    {
+        /* Unity 내에서 기본적으로 Ctrl1, Mouse 왼쪽 버튼은 Fire1로 지정되어 잇음 */
+        if(Input.GetButton("Fire1"))
+        {
+            if(!isAttack)
+            {
+                //Coroutine 실행
+                /* 마우스 클릭을 통한 중복 실행을 방지하기 위함 */
+                StartCoroutine(AttackCoroutine());
+            }
+        }
+    }
+    private bool CheckObject()
+    {
+        /* 
+         * 캐릭터 위치에서 전방으로 레이저를 발사함
+         * transform.forward == transform.TransformDirection(Vector3.forward)
+         * Vector3.forword = (0, 0, 1)
+         *  -> 캐릭터는 위치가 계속해서 변함
+         *  -> 그것을 TransformDirection 함수를 사용해 캐릭터 위치로 변경
+         * 세 번째 매개변수는 충돌체가 있을 경우 hitInfo 변수에 충돌한 오브젝트의 정보를 받아옴
+         * 네 번째 매개변수는 레이저의 길이를 설정
+         */
+        if (Physics.Raycast(transform.position, transform.forward, out hitInfo, currentHand.range))
+            return true;
+        return false;
+    }
+
+    /* 공격 딜레이를 적용하기 위해 Coroutine을 사용 */
+    IEnumerator AttackCoroutine()
+    {
+        isAttack = true;
+        /* 애니메이션에 존재하는 Attack 트리거를 발동시킴 */
+        currentHand.anim.SetTrigger("Attack");
+        /* 공격 상태 적용 딜레이 적용 */
+        yield return new WaitForSeconds(currentHand.attackDelayA);
+        isSwing = true;
+
+        // 공격 활성화 시점
+        StartCoroutine(HitCoroutine());
+
+        /* 공격 상태 제거 딜레이 적용 */
+        yield return new WaitForSeconds(currentHand.attackDelayB);
+        isSwing = false;
+
+        /* 반복적인 공격을 방지하기 위한 딜레이 적용 */
+        yield return new WaitForSeconds(currentHand.attackDelay - currentHand.attackDelayA - currentHand.attackDelayB);
+        isAttack = false;
+    }
+
+    // 공격 Coroutine
+    IEnumerator HitCoroutine()
+    {
+        while(isSwing)
+        {
+            Debug.Log("Attacking");
+            /* 공격을 통해 충돌한 오브젝트가 있는지 확인 */
+            if(CheckObject())
+            {
+                /* 공격에 적중한 오브젝트가 있을 경우 공격 상태를 종료 */
+                isSwing = false;
+                // 공격에 오브젝트가 충돌함
+                Debug.Log("충돌 오브젝트 명 : " + hitInfo.transform.name);
+            }
+            /* 매 프레임 진행 */
+            yield return null;
+        }
+    }
+
+}
